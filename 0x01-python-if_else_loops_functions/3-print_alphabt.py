cc7bdbaeff621e070650b54fdf9fa3651e35a640
Pam-123 <pamschawira@gmail.com>
2024-01-16 12:28:18 -0800

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 
-print("{}".format("".join(chr(ord('a') + i) for i in range(26) if chr(ord('a') + i) not in ('q', 'e'))), end='')
+# Print the lowercase alphabet excluding 'q' and 'e'
+print("{}".format(
+    "".join(chr(ord('a') + i) for i in range(26) if chr(ord('a') + i) not in ('q', 'e'))
+), end='')
