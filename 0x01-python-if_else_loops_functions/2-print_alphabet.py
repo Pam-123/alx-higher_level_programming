56cb8e3944c17d0bec315f66d4ebabe7210d6935
Pam-123 <pamschawira@gmail.com>
2024-01-16 12:20:31 -0800

    2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 
-# ASCII value for 'a'
-ascii_a = ord('a')
-
-# Print the lowercase alphabet using the .format() method
-print("{}".format("".join(chr(ascii_a + i) for i in range(26))), end='')
+print("{}".format("".join(str(i) for i in range(1))), end='')
