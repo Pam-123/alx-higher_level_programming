5b20a3aba90a1f5b3247fafce117b30d09c60d76
Pam-123 <pamschawira@gmail.com>
2024-01-22 10:30:01 -0800

    13

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer to PyObject (Python list)
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, i;
+	PyObject *obj;
+
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %zd\n", size);
+
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+	    obj = PyList_GetItem(p, i);
+	    printf("Element %zd: %s\n", i, Py_TYPE(obj)->tp_name);
+	}
+}
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,50 +7,50 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow, *fast, *prev_slow, *mid_node;
-    listint_t *second_half, *temp;
-    int is_palindrome = 1;
+	listint_t *slow, *fast, *prev_slow, *mid_node;
+	listint_t *second_half, *temp;
+	int is_palindrome = 1;
 
-    slow = fast = *head;
-    prev_slow = NULL;
+	slow = fast = *head;
+	prev_slow = NULL;
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+	    fast = fast->next->next;
 
-        temp = slow;
-        slow = slow->next;
-        temp->next = prev_slow;
-        prev_slow = temp;
-    }
+	    temp = slow;
+	    slow = slow->next;
+	    temp->next = prev_slow;
+	    prev_slow = temp;
+	}
 
-    if (fast != NULL)
-    {
-        mid_node = slow;
-        slow = slow->next;
-    }
+	if (fast != NULL)
+	{
+	    mid_node = slow;
+	    slow = slow->next;
+	}
 
-    second_half = slow;
+	second_half = slow;
 
-    while (prev_slow != NULL && second_half != NULL)
-    {
-        if (prev_slow->n != second_half->n)
-        {
-            is_palindrome = 0;
-            break;
-        }
-        prev_slow = prev_slow->next;
-        second_half = second_half->next;
-    }
+	while (prev_slow != NULL && second_half != NULL)
+	{
+	    if (prev_slow->n != second_half->n)
+	    {
+	        is_palindrome = 0;
+	        break;
+	    }
+	    prev_slow = prev_slow->next;
+	    second_half = second_half->next;
+	}
 
-    while (mid_node != NULL)
-    {
-        temp = prev_slow;
-        prev_slow = prev_slow->next;
-        temp->next = mid_node;
-        mid_node = mid_node->next;
-        temp->next->next = temp;
-    }
+	while (mid_node != NULL)
+	{
+	    temp = prev_slow;
+	    prev_slow = prev_slow->next;
+	    temp->next = mid_node;
+	    mid_node = mid_node->next;
+	    temp->next->next = temp;
+	}
 
-    return is_palindrome;
+	return (is_palindrome);
 }
