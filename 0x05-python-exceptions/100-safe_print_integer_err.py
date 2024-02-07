5a58c8fe438858747ecffa8cf7ea9c139ac8d4a2
Pam-123 <pamschawira@gmail.com>
2024-02-07 11:10:56 -0800

    All

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
+
+import sys
+
+
 def safe_print_integer_err(value):
+    """Prints an integer with "{:d}".format().
+
+    If a ValueError message is caught, a corresponding
+    message is printed to standard error.
+
+    Args:
+        value (int): The integer to print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True.
+    """
     try:
-        # Attempt to convert the value to an integer
-        integer_value = int(value)
-        # Print the integer followed by a new line
-        print("{:d}".format(integer_value))
-        return True
-    except ValueError:
-        # Handle the case where the value is not an integer
-        import sys
-        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
-        return False
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
