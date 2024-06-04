1252a72c82c5040fa1acb836061b7e41ea8288ea
Pam-123 <pamschawira@gmail.com>
2024-06-04 13:59:10 -0700

    Yes

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,5 @@
-#!/usr/bin/env node
-
+#!/usr/bin/node
 const fs = require('fs');
-
-// Get the file path and string to write from the arguments
-const filePath = process.argv[2];
-const stringToWrite = process.argv[3];
-
-// Write the string to the file in utf-8 encoding
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-    if (err) {
-        // If an error occurred during writing, print the error object
-        console.log(err);
-        return;
-    }
-    // Optional: Print a success message
-    console.log(`Successfully wrote to ${filePath}`);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
